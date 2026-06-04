5b186fa67ea5b86148216941019706ac57597a1c
Mohamed Sabeek <safeesafee0000@gmail.com>
2026-06-04 12:45:23 +0530

    chore: update browser header icon to use profile image

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
+import profileImg from './public/profile.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/vitejs/vite/main/packages/vite/src/node/server/public/vite.svg" />
+      <link rel="icon" type="image/png" href={profileImg} />
       <App />
     </HelmetProvider>
   </React.StrictMode>,
